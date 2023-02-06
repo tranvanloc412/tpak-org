04e46c52598f77eb62e43e314b5fbb5899e4df80
Loc Tran Van <loctranvan@Locs-MacBook-Air.local>
2023-02-07 06:59:38 +0700

    create repos in tpak-org

--- repos.tf
+++ repos.tf
@@ -1,3 +1,19 @@
-resource "github_repository" "test" {
-  name = "some-repo"
+resource "github_repository" "authentication_service" {
+  name = "authentication-service"
+}
+
+resource "github_repository" "broker_service" {
+  name = "broker-service"
+}
+
+resource "github_repository" "listener_service" {
+  name = "listener-service"
+}
+
+resource "github_repository" "logger_service" {
+  name = "logger-service"
+}
+
+resource "github_repository" "mail_service" {
+  name = "front-end"
 }
