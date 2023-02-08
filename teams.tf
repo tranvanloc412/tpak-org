17f23dd3cb216327d4de83e01c5b5bcf8ae73b9e
Loc Tran Van <loctranvan@Locs-MacBook-Air.local>
2023-02-08 15:45:02 +0700

    add team member

--- teams.tf
+++ teams.tf
@@ -2,3 +2,9 @@ resource "github_team" "my_team" {
   name        = "my-team"
   description = "My cool team"
 }
+
+resource "github_team_membership" "my_team_membership" {
+  team_id  = "${github_team.my_team.id}"
+  username = "vanloc132"
+  role     = "member"
+}
